59821815e8e3b01c983e288467a32e09c13175bc
solo909 <steven.solorzano@csu.fullerton.edu>
2026-04-13 14:50:51 -0700

    Updated main to print baseline result

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from parser import parse_log
 from features import build_features
+from baseline import rule_based_detection
 
 file_path = "data/auth.log"
 
@@ -12,4 +13,12 @@ features_df = build_features(df)
 print("FEATURE DATA")
 print(features_df)
 print()
-print("Total IPs:", len(features_df))
+
+result_df = rule_based_detection(features_df)
+print ("BASELINE RESULT")
+print(result_df)
+print()
+
+alerts = result_df[result_df["rule_flag"] == 1]
+print("SUSPICIOUS IPS")
+print(alerts)
