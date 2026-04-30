9009149352c0724ff13322a5ef6744e9ec76d989
solo909 <steven.solorzano@csu.fullerton.edu>
2026-04-29 19:13:21 -0700

    Upload file feature created to support log import

--- app.py
+++ app.py
@@ -8,3 +8,20 @@ from baseline import rule_based_detection
 from models import run_isolation_forest, run_dbscan
 
 st.title("SSH Log Anomally Detection")
+
+# For file uploading (capability for user)
+uploaded_file = st.file_uploader("Upload auth.log", type=["log"])
+
+if uploaded_file:
+
+        df = parse_log(uploaded_file)           # Runs parse on uploaded file
+
+        st.subheader("Raw Log Data")            # Header
+        st.write(df)                            # Displays raw parsed data
+
+        featrues_df = build_features(df)        # Brings features together for display
+
+        st.subheader("Feature Data")
+        st.write(featrues_df)                   # Displaying features
+
+        
